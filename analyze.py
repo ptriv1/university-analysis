bbaf946bc992792cb24752ee54afa8ad8123fda5
PT <trivedp18@gmail.com>
2025-04-01 14:32:38 -0400

    changes to analyze.py for names of columns

--- analyze.py
+++ analyze.py
@@ -1,5 +1,6 @@
 import pandas as pd
 
-df = pd.read_csv("world-universities.csv")
+df = pd.read_csv("world-universities.csv", header=None)
+df.columns = ["country_code", "university_name", "website"]
 
 print(df.head())
