1d42bc8154e3b01ed3f0737e17f42ab600fef067
PT <trivedp18@gmail.com>
2025-04-01 14:39:51 -0400

    grouped universities

--- analyze.py
+++ analyze.py
@@ -3,4 +3,5 @@ import pandas as pd
 df = pd.read_csv("world-universities.csv", header=None)
 df.columns = ["country_code", "university_name", "website"]
 
-print(df.head())
+counts = df.groupby("country_code").size()
+print(counts)
