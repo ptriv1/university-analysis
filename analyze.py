a6879bdae06b19864fe029ba93a693fb3792b250
PT <trivedp18@gmail.com>
2025-04-02 13:27:45 -0400

    merged two datasets

--- analyze.py
+++ analyze.py
@@ -1,6 +1,8 @@
 import pandas as pd
 
-df = pd.read_csv("world-universities.csv")
+df_universities = pd.read_csv("world-universities.csv")
+df_continents = pd.read_csv("continents2.csv")
 
-counts = df.groupby("country_code").size()
-print(counts)
+df_merged = pd.merge(df_universities, df_continents, on="country_code", how="left")
+
+print(df_merged.head())
