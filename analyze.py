85ff46c708d326d7901df625add2494148417a13
PT <trivedp18@gmail.com>
2025-04-02 13:19:35 -0400

    changed analyze.py because of changes in world-universities.csv

--- analyze.py
+++ analyze.py
@@ -1,7 +1,6 @@
 import pandas as pd
 
-df = pd.read_csv("world-universities.csv", header=None)
-df.columns = ["country_code", "university_name", "website"]
+df = pd.read_csv("world-universities.csv")
 
 counts = df.groupby("country_code").size()
 print(counts)
